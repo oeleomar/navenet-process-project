d8ab0b66a59b687ea5f06065b0e5b0c50ee42356
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2022-11-16 09:10:20 -0400

    Initial config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,14 +9,4 @@ export default defineConfig({
     outDir: '../dist',
   },
   publicDir: '../public',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['../.test/setup.js'],
-    include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'templates'],
-    // coverage: {
-    //   reporter: ['clover', 'json', 'lcov'],
-    // },
-  },
 });
